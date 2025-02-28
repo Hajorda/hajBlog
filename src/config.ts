72b5c3f979b2c0aeb84b4b6bcb3af51c56d480c4
Hajorda <hajorda@gmail.com>
2025-02-28 15:15:38 +0300

    resume eddited

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "HajBlog";
+export const SITE_TITLE = "AliBolat Blog";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog!";
+  "Ali Bolat's personal blog. I write about software development, web development, and other tech-related topics. Flutter, React, Node.js, Java and more.";
 export const TWITTER_HANDLE = "@hajorda1";
 export const MY_NAME = "Hajorda";
 
